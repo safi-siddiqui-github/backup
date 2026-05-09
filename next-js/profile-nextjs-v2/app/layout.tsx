c30287cf5c7910ab8e5ee5eeae3b48b891457ad8
safi-siddiqui-github <safisiddiqui.work@gmail.com>
2026-05-09 12:56:03 +0500

    wip

---
+++ next-js/profile-nextjs-v2/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Safi Siddiqui',
+  description: 'Profile',
+  keywords: [
+    'web',
+    'app',
+    'mobile',
+    'front',
+    'back',
+    'full',
+    'stack',
+    'html',
+    'css',
+    'javascript',
+    'typescript',
+    'react',
+    'next',
+    'vue',
+    'php',
+    'laravel',
+    'livewire',
+    'inertia',
+    'native',
+    'expo',
+    'profile',
+  ],
+};
+
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body className="antialiased text-sm">
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
